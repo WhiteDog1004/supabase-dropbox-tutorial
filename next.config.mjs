1608d95f9704839459e825b8893317380906b108
WhiteDog1004 <dlrlgus1204@naver.com>
2025-02-05 21:25:24 +0900

    drop-6 : image hostname 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "dmbvejcxnpuhpprnykgm.supabase.co",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+};
 
 export default nextConfig;
